7018b4f2bb1018ee0abc7fd45ad44194b2749e9f
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-02-06 14:01:17 -0500

    add github selector

--- src/helpers/Hover.tsx
+++ src/helpers/Hover.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HoverContext } from './useHover';
 
+// TODO: turn this into a HOC?
 // Wrapper that keeps track of weather or not the component is being hovered
 export const Hover: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   children,
---
+++ stories/LegacyGithubSelectorEmoji.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+import { GithubSelectorEmoji } from '../legacy/src/components/github/GithubSelectorEmoji';
+
+const meta: Meta = {
+  title: 'LegacyGithubSelectorEmoji',
+  component: GithubSelectorEmoji,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story<any> = args => <GithubSelectorEmoji {...args} />;
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const Default = Template.bind({});
+
+Default.args = {
+  shortcode: '😄',
+};
