7f9baead486e6bebbec524db30d4fe80b241da99
Louise Ann Apostol <louiseann.apostol@gmail.com>
2021-09-18 17:27:52 +0800

    ReactionCounter component

--- src/components/custom/ReactionCounter.tsx
+++ src/components/custom/ReactionCounter.tsx
@@ -1,31 +1,71 @@
 import React from 'react';
-import { groupBy, ReactionCounterObject } from '../../helpers';
+import { groupBy, listOfNames, ReactionCounterObject } from '../../helpers';
+import { ReactionCounterEmoji, ReactionCounterEmojiProps } from './ReactionCounterEmoji';
 
-export interface ReactionCounterProps {
+export interface ReactionCounterProps extends Partial<Pick<ReactionCounterEmojiProps, 'iconSize' | 'bg'>> {
   reactions: ReactionCounterObject[];
   user?: string;
   important?: string[];
-  onlyShowTotal?: boolean;
-  showOthers?: boolean;
+  showTotalOnly?: boolean;
+  showOthersAlways?: boolean;
   className?: string;
   onClick?: () => void;
 }
 
 export const ReactionCounter: React.VFC<ReactionCounterProps> = ({
   reactions,
+  user,
+  important,
   className,
   onClick,
-  onlyShowTotal = false,
-  showOthers = true,
+  iconSize = 24,
+  bg = '#FFF',
+  showTotalOnly = false,
+  showOthersAlways = true,
 }) => {
   const groups = groupBy(reactions, 'label');
   const names = reactions.map(({ by }: ReactionCounterObject) => {
     return by;
   });
 
+  const nameString = [];
+  if (user && names.includes(user)) {
+    nameString.push('You');
+  }
+  if (important?.length) {
+    if (names.includes(important[0])) {
+      nameString.push(important[0]);
+    }
+    if (names.includes(important[1])) {
+      nameString.push(important[1]);
+    }
+  }
+  const othersCount = names.length - nameString.length;
+  if (showOthersAlways || othersCount > 0) {
+    nameString.push(`${othersCount} other${othersCount === 1 ? '' : 's'}`);
+  }
+
+  const nameStyle: React.CSSProperties = React.useMemo(() => {
+    return {
+      fontSize: `${iconSize - 8}px`,
+      marginLeft: '8px',
+    };
+  }, [iconSize]);
+
   return (
     <div style={counterStyle} className={className} onClick={onClick}>
-      reaction counter
+      {Object.keys(groups).map((reaction, i, reactions) => (
+        <ReactionCounterEmoji
+          key={i}
+          bg={bg}
+          iconSize={iconSize}
+          index={reactions.length - i}
+          node={groups[reaction][0].node}
+        />
+      ))}
+      <div style={nameStyle}>
+        {showTotalOnly ? names.length : listOfNames(nameString)}
+      </div>
     </div>
   );
 };
@@ -33,6 +73,7 @@ export const ReactionCounter: React.VFC<ReactionCounterProps> = ({
 const counterStyle = {
   display: 'flex',
   cursor: 'pointer',
+  alignItems: 'center',
 };
 
 export default ReactionCounter;
