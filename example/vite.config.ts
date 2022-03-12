4dd349d53fcbe7945ae0ab11e608474d06c9443d
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-03-12 15:29:15 -0500

    some minor changes

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // https://github.com/vitejs/vite/issues/6215#issuecomment-1064247764
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+    },
+  },
+});
